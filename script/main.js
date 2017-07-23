f4349caa0b496b0906d7223df9f03249c16483b5
Andrew Klapp <andklapp@null.net>
2017-07-22 23:34:19 -0500

    Added pretty transitions for the menu.

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('#hamburger-button, nav a').click(function () {
-        $('#nav-links').toggle();
+        $('#nav-links').toggle('swing');
     });
 
     $('nav').mouseleave(function () {
-        $('#nav-links').hide();
+        $('#nav-links').hide('swing');
     });
 });
