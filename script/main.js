610cef205bd808f14cdbe0be89f6693298a95b60
Andrew Klapp <andklapp@null.net>
2017-07-23 00:08:02 -0500

    Menu now switches properly.

--- script/main.js
+++ script/main.js
@@ -1,9 +1,33 @@
+
+menu_collapse_width = 720;
+
 $(document).ready(function () {
+    switch_menus_if_small_window();
+
     $('#hamburger-button, nav a').click(function () {
         $('#nav-links').toggle('swing');
     });
 
-    $('nav').mouseleave(function () {
-        $('#nav-links').hide('swing');
+    $(window).resize(function () {
+        switch_menus_if_small_window();
     });
 });
+
+function switch_menus_if_small_window() {
+    if($(window).width() <= menu_collapse_width) {
+        // Switch to hamburger mode
+        $('#hamburger-button').show();
+        $('#nav-links').addClass('collapsed-nav-links');
+        $('nav').mouseleave(function () {
+            $('#nav-links').hide('swing');
+        });
+    }
+    else {
+        // Switch to normal menu mode
+        $('nav').unbind('mouseleave');
+        $('#hamburger-button').hide();
+        $('#nav-links').removeClass('collapsed-nav-links');
+        $('#nav-links').show();
+
+    }
+}
